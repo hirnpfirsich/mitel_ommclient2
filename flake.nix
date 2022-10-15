607430ca691d8bead4f437dfda5a3b7815bac4a6
clerie <git@clerie.de>
2022-10-15 16:51:07 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }: {
+    packages.x86_64-linux = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+    in {
+      mitel-ommclient2 = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "mitel-ommclient2";
+        version = "0.0.1";
+
+        src = ./.;
+
+        format = "pyproject";
+
+        buildInputs = [ pkgs.python3Packages.hatchling ];
+        propagatedBuildInputs = [ pkgs.python3Packages.rsa ];
+
+        pythonImportsCheck = [ "mitel_ommclient2" ];
+      };
+    };
+
+    hydraJobs = {
+      inherit (self)
+        packages;
+    };
+  };
+}
